c41dba4c5da778dcb2e6f3733bf7151c0bfab94c
Philip Ingram <neodestro734@yahoo.com>
2014-04-16 22:38:49 -0700

    Tag#most_popular_rails better but still broken

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -28,8 +28,26 @@ class Tag < ActiveRecord::Base
   end
 
   def most_popular_rails
-    ShortenedUrl.joins(:visits).group("visits.shortened_id").order(
-      "COUNT(visits.id)").where("tag_id", self.id)limit(5)
+     # posts_with_counts = self
+     #  .posts
+     #  .select("posts.*, COUNT(*) AS comments_count") # more in a sec
+     #  .joins(:comments)
+     #  .group("posts.id")
+
+    links_with_counts = self
+      .tag_links
+      .select("tag_links.*, COUNT(*) AS tags_count")
+      .joins(:link)
+      .group("shortened_urls.id")
+
+
+    # ShortenedUrl.joins(:visits).group("visits.shortened_id").order(
+    #   "COUNT(visits.id)").where("tag_id", self.id)limit(5)
+
+    # self.tag_links#.includes(:link)
+    #   .select("tag_links.*, COUNT(*) AS count")
+    #   .joins(:link)
+    #   .group("tag_links.tag_id")
   end
 
 end
